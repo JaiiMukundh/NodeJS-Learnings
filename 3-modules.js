f939c60ca75c6de61dcc7dabab023cb89d29255d
JaiiMukundh <ajaiimukundh@gmail.com>
2025-08-26 18:07:27 +0530

    Add files via upload

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const names = require('./4-names');
+
+const sayHi = require('./5-modules');
+const eou = require('./6-alternativeflavour');
+
+require('./7-mindgrenade');
+// console.log(eou);
+// sayHi(names.jaii);
+// sayHi(names.shs);
---
+++ 4-names.js
@@ -0,0 +1,5 @@
+
+const jaii = "jaii";
+const shs = "kohls";
+
+module.exports  = {jaii,shs};
